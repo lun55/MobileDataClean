ff423ed8326f2393f3f807f0d61c4e60c6d7c7d8
lmq <lunmaoqi@qq.com>
2025-06-11 12:02:52 +0800

    pop

---
+++ PopStus.py
@@ -0,0 +1,102 @@
+import os
+import glob
+import re
+from multiprocessing import Pool
+from tqdm import tqdm
+import pandas as pd
+import geopandas as gpd
+from easydict import EasyDict as edict
+
+'''
+    统计区域单元内的人口数量
+'''
+
+class Pop():
+    def __init__(self, config):
+        self.Area_path = config.Area_path  
+        self.input_folder = config.input_folder
+        self.output_folder = config.output_folder
+        self.if_month = config.if_month # 是否按月份输出文件
+        self.processes = config.processes
+        self.Area = gpd.read_file(self.Area_path)
+        self.Area.to_crs(epsg=4326, inplace=True)
+
+
+    def process(self):
+        # 获取所有CSV文件
+        csv_files = glob.glob(os.path.join(self.input_folder, "**/*.csv"), recursive=True)
+        
+        # 使用多进程处理文件
+        with Pool(processes=self.processes) as pool:
+            # 使用实例方法包装函数，以便传递self
+            list(tqdm(pool.imap(self._process_file, csv_files), 
+                     total=len(csv_files), 
+                     desc="处理文件中"))
+
+
+    def _process_file(self, csv_file):
+        """
+        处理单个CSV文件
+        """
+        # 从文件路径中提取日期信息
+        basename = os.path.basename(csv_file)
+        month = ""
+        study_area = ""
+        if self.if_month:
+            match = re.search(r"\\(\d+月)\\", csv_file)
+            if match:
+                month = match.group(1)
+            else:
+                match = re.search(r"_(\d+)_", basename)  # 匹配 _数字_ 部分
+                if match:
+                    month = match.group(1) + "月"
+                else:
+                    print("未找到月份")
+        
+        area_match = re.search(r"\\漂移清洗\\([^\\]+)", csv_file)
+        if area_match:
+            study_area = area_match.group(1)
+  
+        # 动态构建路径层级（跳过空值）
+        path_parts = [self.output_folder]
+        if study_area: path_parts.append(study_area)
+        if month: path_parts.append(month)
+        output_folder = os.path.join(*path_parts)
+        # 确保输出文件夹存在
+        os.makedirs(output_folder, exist_ok=True)
+        output_file = os.path.join(output_folder, basename)
+        if os.path.exists(output_file):
+            print(f"已存在 {output_file}")
+            return
+
+        df = pd.read_csv(csv_file)
+        # 将数据块转换为GeoDataFrame
+        gdf = gpd.GeoDataFrame(df, geometry=gpd.points_from_xy(df.longitude, df.latitude))
+        gdf.crs = "EPSG:4326"
+        # 空间连接
+        new_points = gpd.sjoin(gdf, self.Area, how='left')
+        new_points.dropna(subset=['FID'], inplace=True)
+        new_points['FID'] = new_points['FID'].astype(int)
+        new_points = new_points[['ID','timestamp','FID']]
+        # 时间转码
+        new_points['timestamp'] = pd.to_datetime(new_points['timestamp'], format='%Y-%m-%d %H:%M:%S')
+        new_points["time"] = new_points["timestamp"].dt.hour * 4 + new_points["timestamp"].dt.minute // 15
+        # 聚类统计区域的人流量
+        new_points.drop('timestamp', axis=1, inplace=True) # 直接修改原 DataFrame
+        population_counts = new_points.groupby(['FID', 'time'])['ID'].nunique().reset_index(name='population')       
+        population_counts.to_csv(output_file, index=False, header=True)
+
+
+if __name__ == "__main__":
+
+    cities = ["福州", "厦门", "漳州", "泉州", "宁德", "莆田"]  # 所有城市列表
+    for area in cities:
+        pop_config = edict({
+            'Area_path': fr"E:\四大城市\格网\200\{area}.shp",  # 城市格网矢量文件路径
+            'input_folder': f'H:\结果数据\停留点\{area}',  # 停留点数据路径
+            'output_folder': r'H:\结果数据\格网映射\200',  # OD文件输出路径
+            'if_month': True,  # 是否按照月份分类
+            'processes': 5  # 并发进程数量
+        })
+        # 处理当前城市
+        Pop(pop_config).process()
--- main.py
+++ main.py
@@ -10,18 +10,18 @@ from easydict import EasyDict as edict
 if __name__ == "__main__":
 
     dataset = 'Timing' # 数据集
-    area = '' # 研究区
-    month = '' # 月份
+    area = '福州' # 研究区
+    month = '4' # 月份
     geo_config = edict({
-        'Area_path': f"H:\研究区\{area}.shp",  # 城市矢量文件路径
-        'input_folder': r"", # 输入数据路径 
+        'Area_path': f"H:\研究区\原始文件\{area}.shp",  # 城市矢量文件路径
+        'input_folder': r"G:\2023-04-03", # 输入数据路径 
         'dataset': dataset,
-        'output_folder': f"H:\处理\区域筛选\{area}\{dataset}\{month}"
+        'output_folder': f"H:\{area}\{dataset}\{month}"
     })
     draft_config = edict({
         'db_path': f'D:\data_draft_{area}.db', 
-        'input_folder': fr"H:\处理\区域筛选\{area}\{dataset}\{month}",
-        'output_folder': fr'E:\四大城市\处理\漂移点清洗\{area}\{dataset}\{month}',
+        'input_folder': fr"H:\{area}\{dataset}\{month}",
+        'output_folder': fr"H:\{area}\{dataset}\{month}\漂移",
         'batch_size': 300000 # 每此处理的批次大小
     })
     stay_config = edict({
@@ -32,16 +32,16 @@ if __name__ == "__main__":
         'batch_size': 300000 # 每此处理的批次大小
     })
     # # # 区域筛选
-    print("-"*10 + "区域筛选"+"-"*20)
-    GeographicF(geo_config).process()
+    # print("-"*10 + "区域筛选"+"-"*20)
+    # GeographicF(geo_config).process()
     # # 漂移点清洗
     print("-"*10 + "漂移点清洗"+"-"*20)
-    # DriftData(draft_config).process()
-    # # 内存不足时，使用下面这个
     DriftData(draft_config).process_sql()
+    # # 内存不足时，使用下面这个
+    # DriftData(draft_config).process_sql()
     # # # 停留点识别
-    print("-"*10 + "停留点识别"+"-"*20)
+    # print("-"*10 + "停留点识别"+"-"*20)
     # StayPoint(stay_config).process()
     # # 内存不足时，停留点识别时使用下面这个
-    StayPoint(stay_config).process_sql() 
+    # StayPoint(stay_config).process_sql() 
     
